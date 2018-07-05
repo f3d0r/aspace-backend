130eb108c1b2e43044b92ac6239c3a177cab9f48
Fedor Paretsky <fedor@trya.space>
2018-07-04 17:33:52 -0700

    Add parking update status endpoint and auth_key, update npm packages

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 // IMPORTS
 const express = require('express');
-var app = express();
-var auth = express();
-var parking = express();
 const uniqueString = require('unique-string');
 const request = require("request");
 const mysql = require('mysql');
@@ -18,30 +15,32 @@ const DATABASE_NAME = 'aspace';
 const DATABASE_PORT = 'db_port';
 const SOCKET_PATH = '/var/run/mysqld/mysqld.sock';
 
-const API_PORT = '5000';
+const API_PORT = 3000;
 
 // CONNECTION AND CLIENT SET UP
 
 const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 var connection = mysql.createConnection({
-    host: '127.0.0.1',
+    host: '159.65.70.74',
     user: DATABASE_USER,
     password: DATABASE_PASSWORD,
     database: DATABASE_NAME,
     port: DATABASE_PORT,
-    socketPath: SOCKET_PATH
+    // socketPath: SOCKET_PATH
 });
-connection.connect();
 
 connection.connect(function (err) {
     if (err) {
-        console.error('error connecting: ' + err.stack);
+        console.error('MySQL connection error : ' + err.stack);
         return;
     }
-    console.log('connected as id ' + connection.threadId);
-}); 
+    console.log('MySQL successfully connected!');
+});
 
+var app = express();
+var auth = express();
+var parking = express();
 app.use(express.json());
 app.use('/auth', auth);
 app.use('/parking', parking);
@@ -61,29 +60,53 @@ parking.get('/', function (req, res) {
     res.status(200).send("This is the parking sub-API for aspace! :)");
 });
 
-parking.get('/get_spots', function (req, res) {
+parking.post('/update_status', function (req, res) {
+    if (!queryExists(req, 'auth_key')) {
+        sendErrorJSON(res, 'MISSING_AUTH_KEY');
+    } else if (!queryExists(req, 'spot_id')) {
+        sendErrorJSON(res, 'MISSING_PARAMETER', 'spot_id required');
+    } else if (!queryExists(req, 'occupied')) {
+        sendErrorJSON(res, 'MISSING_PARAMETER', 'occupied required');
+    } else {
+        var sql = "SELECT * FROM `database_authority` WHERE `auth_key` = " + escapeQuery(req.query.auth_key) + " AND `permission` = 'update_spots'";
+        connection.query(sql, function (error, rows) {
+            if (rows.length == 1) {
+                var sql = "UPDATE `parking` set `occupied` = " + escapeQuery(req.query.occupied) + " WHERE `spot_id` = " + escapeQuery(req.query.spot_id);
+                connection.query(sql, function (error, results, fields) {
+                    if (results.affectedRows == 0) {
+                        sendErrorJSON(res, 'INVALID_SPOT_ID');
+                    } else {
+                        res.status(200).send("spot_id successfully updated!");
+                    }
+                });
+            } else {
+                sendErrorJSON(res, 'INVALID_AUTH_KEY');
+            }
+        });
+    }
+});
+
+parking.get('/get_status', function (req, res) {
     if (!queryExists(req, 'block_id') && !queryExists(req, 'spot_id')) {
         sendErrorJSON(res, 'MISSING_PARAMETER', 'block_id or spot_id required');
-    } else if (req, queryExists('block_id')) {
-        connection.query('SELECT * from parking where block_id = ' + req.query.block_id, function (error, results, fields) {
+    } else if (!queryExists(req, 'block_id')) {
+        connection.query('SELECT * from parking where spot_id = ' + escapeQuery(req.query.spot_id), function (error, results, fields) {
             if (error) {
-                res.send(500).send(error);
+                res.status(500).send(error);
             }
             res.status(200).send(JSON.stringify(results));
         });
-    } else if (req, queryExists('spot_id')) {
-        if (req, queryExists('spot_id')) {
-            connection.query('SELECT * from parking where spot_id = ' + req.query.spot_id, function (error, results, fields) {
-                if (error) {
-                    res.send(500).send(error);
-                }
-                res.status(200).send(JSON.stringify(results));
-            });
-        }
-    } else {
-        connection.query('SELECT * from parking where block_id = ' + req.query.block_id + ' and spot_id = ' + req.query.spot_id, function (error, results, fields) {
+    } else if (!queryExists(req, 'spot_id')) {
+        connection.query('SELECT * from parking where block_id = ' + escapeQuery(req.query.block_id), function (error, results, fields) {
             if (error) {
-                res.send(500).send(error);
+                res.status(500).send(error);
+            }
+            res.status(200).send(JSON.stringify(results));
+        });
+    } else {
+        connection.query('SELECT * from parking where block_id = ' + escapeQuery(req.query.block_id) + ' and spot_id = ' + escapeQuery(req.query.spot_id), function (error, results, fields) {
+            if (error) {
+                res.status(500).send(error);
             }
             res.status(200).send(JSON.stringify(results));
         });
@@ -97,14 +120,13 @@ parking.post('/add_points', function (req, res) {
 });
 
 parking.post('/check_block_id_exists', function (req, res) {
-    connection.query('SELECT * from parking where block_id = ' + req.query.block_id, function (error, results, fields) {
+    connection.query('SELECT * from parking where block_id = ' + escapeQuery(req.query.block_id), function (error, results, fields) {
         if (error) throw error;
         res.send(results.length == 0 ? "F" : "T");
     });
 });
 
 function insertSpot(points, current) {
-    console.log(points[current]);
     var query = connection.query('INSERT INTO parking SET ?', points[current], function (error, results, fields) {
         if (error) throw error;
     });
@@ -122,93 +144,70 @@ auth.post("/phone_login", function (req, res) {
     if (!queryExists(req, 'phone_number')) {
         sendErrorJSON(res, 'MISSING_PARAMETER', 'phone_number required');
     } else {
-        lookupPhone(req.query.phone, function (successInfo) {
-            console.log(successInfo.phone_number);
+        lookupPhone(req.query.phone_number, function (response) {
+            var sql = "SELECT * FROM users WHERE phone_number = " + escapeQuery(response.phone_number, true);
+            connection.query(sql, function (error, rows) {
+                if (rows.length == 0) {
+                    var userId = uniqueString();
+                    var code = Math.floor(100000 + Math.random() * 900000);
+                    var sql = "INSERT INTO `verification_codes` (`user_id`, `phone_number`, `verify_code`) VALUES ('" + escapeQuery(userId, false) + "', '" + escapeQuery(response.phone_number, false) + "', '" + escapeQuery(code, false) + "')";
+                    connection.query(sql, function (error, rows) {
+                        sendErrorJSON(res, 'NEW_PHONE');
+                        sendVerificationText(response.phone_number, code);
+                    });
+                } else {
+                    sendErrorJSON(res, 'RETURN_PHONE');
+                }
+            });
         }, function () {
             sendErrorJSON(res, 'INVALID_PHONE');
         });
     }
-    
-
-    // lookupPhones(
-    //     client.lookups.phoneNumbers(req.query.phone_number)
-    //     .fetch()
-    //     .then(phone_number => {
-    //         var sql = "SELECT * FROM ?? WHERE ?? = ?";
-    //         var inserts = ['users', 'phone', phone_number.phoneNumber];
-    //         sql = mysql.format(sql, inserts);
-    //         connection.query(sql, function (error, rows) {
-    //             if (rows.length == 0) {
-    //                 var userId = uniqueString();
-    //                 var code = Math.floor(100000 + Math.random() * 900000);
-    //                 var sql = "INSERT INTO `verification_codes`(`userId`, `code`) VALUES ('" + userId + "', '" + code + "')"
-    //                 console.log("SQL : " + sql);
-    //                 connection.query(sql, function (error, rows) {
-    //                     res.send("101");
-    //                     sendText(phone_number.phoneNumber, code);
-    //                 });
-    //             } else {
-    //                 res.send("102");
-    //             }
-    //         });
-    //     })
-    //     .done();
-    // })
-});
-
-auth.get('/phone_lookup', function (req, res) {
-    lookupPhone(req.query.phone, function (successInfo) {
-        // SUCCESS CALLBACK
-        console.log(successInfo.phone_number);
-    }, function (errorCode) {
-        // ERROR CALLBACK
-        console.log("ERROR : " + errorCode);
-    });
-    res.status(200).send("OK");
 });
 
 auth.post("/check_pin", function (req, res) {
-    var phoneNumber = req.query.phone_number;
-    var code = req.query.pin;
-    client.lookups.phoneNumbers(phoneNumber)
-        .fetch()
-        .then(phone_number => {
-            var sql = "SELECT * FROM ?? WHERE ?? = ? AND ?? = ?";
-            var inserts = ['verification_codes', 'phone', phone_number.phoneNumber, 'code', code];
-            sql = mysql.format(sql, inserts);
-            console.log("SQL : " + sql);
+    if (!queryExists(req, 'phone_number')) {
+        sendErrorJSON(res, 'MISSING_PARAMETER', 'phone_number required');
+    } else if (!queryExists(req, 'verify_pin')) {
+        sendErrorJSON(res, 'MISSING_PARAMETER', 'verify_pin required');
+    } else {
+        lookupPhone(req.query.phone_number, function (response) {
+            var sql = "SELECT * FROM `verification_codes` WHERE `phone` = " + escapeQuery(response.phone_number) + " AND `code` = " + escapeQuery(req.query.verify_pin);
             connection.query(sql, function (error, rows) {
                 if (rows.length == 1) {
                     res.send("{\"userId\" : \"" + rows[0].userId + "\"}");
                 } else {
-                    res.send("-1");
+                    sendErrorJSON(res, 'INVALID_PIN');
                 }
             });
-        })
-        .done();
-
+        }, function () {
+            sendErrorJSON(res, 'INVALID_PHONE');
+        });
+    }
 })
 
 // MISC FUNCTIONS
-function sendText(phoneNumber, pin) {
+function sendVerificationText(phoneNumber, pin) {
     client.messages
         .create({
-            body: "Your aspace verification code is: " + pin + " - do not share this code with anyone.",
+            body: "aspace code: " + pin + ". Happy Parking! :)",
             from: 'twilio_origin_phone_number',
             to: phoneNumber
         })
         .done();
 }
 
-function sendErrorJSON(res, error, missing_parameter) {
+function sendErrorJSON(res, error, extraJSON) {
     var responseJSON = {
         error: {
             error_code: ERROR_CODES[error].ERROR_CODE,
             error_info: ERROR_CODES[error].INFO
         }
     };
-    if (missing_parameter != null && error == 'MISSING_PARAMETER') {
-        responseJSON.error['missing_parameter'] = missing_parameter;
+    if (typeof extraJSON != 'undefined' && extraJSON != null && error == 'MISSING_PARAMETER') {
+        responseJSON.error['missing_parameter'] = extraJSON;
+    } else if (typeof extraJSON != 'undefined' && extraJSON != null) {
+        responseJSON.error['info'] = extraJSON;
     }
     res.status(ERROR_CODES[error].HTTP_CODE).send(responseJSON);
 }
@@ -218,6 +217,10 @@ function queryExists(req, queryName) {
         return false;
     }
     return true;
+}
+
+function escapeQuery(query) {
+    return connection.escapeId(query).split('`').join('\'');
 }
 
 function lookupPhone(phoneNumber, successCallBack, failCallBack) {
@@ -243,13 +246,11 @@ function lookupPhone(phoneNumber, successCallBack, failCallBack) {
             successCallBack(body);
         }
     });
-
 }
 
 // SERVER SET UP
 app.listen(API_PORT, function () {
     console.log("Server has started on port " + API_PORT + "!");
 })
-
 
 module.exports = app;
--- errorCodes.js
+++ errorCodes.js
@@ -4,53 +4,79 @@ module.exports = Object.freeze({
         ERROR_CODE: -1,
         INFO: "missing_parameter"
     },
-    PHONE_DOESNT_EXIST: {
-        HTTP_CODE: 400,
-        ERROR_CODE: 0,
-        INFO: "phone_doesnt_exist"
+    NEW_PHONE: {
+        HTTP_CODE: 200,
+        ERROR_CODE: 1,
+        INFO: "new_phone"
+    },
+    RETURN_PHONE: {
+        HTTP_CODE: 200,
+        ERROR_CODE: 2,
+        INFO: "return_phone"
     },
     INVALID_PASSWORD: {
-        ERROR_CODE: 1,
+        HTTP_CODE: 200,
+        ERROR_CODE: 3,
         INFO: "invalid_password"
     },
     INVALID_PIN: {
-        ERROR_CODE: 2,
+        HTTP_CODE: 200,
+        ERROR_CODE: 4,
         INFO: "invalid_pin"
     },
     DB_ERROR: {
-        ERROR_CODE: 3,
+        HTTP_CODE: 200,
+        ERROR_CODE: 5,
         INFO: "db_error"
     },
     NOT_FOUND: {
-        ERROR_CODE: 4,
+        HTTP_CODE: 200,
+        ERROR_CODE: 6,
         INFO: "not_found"
     },
-    PHONE_ALREADY_EXISTS: {
-        ERROR_CODE: 5,
-        INFO: "phone_already_exists"
-    },
     COULD_NOT_CREATE_USER: {
-        ERROR_CODE: 6,
+        HTTP_CODE: 200,
+        ERROR_CODE: 7,
         INFO: "could_not_create_user"
     },
     PASSWORD_RESET_EXPIRED: {
-        ERROR_CODE: 7,
+        HTTP_CODE: 200,
+        ERROR_CODE: 8,
         INFO: "password_reset_expired"
     },
     PASSWORD_RESET_HASH_MISMATCH: {
-        ERROR_CODE: 8,
+        HTTP_CODE: 200,
+        ERROR_CODE: 9,
         INFO: "password_reset_hash_mismatch"
     },
     PASSWORD_RESET_PHONE_MISMATCH: {
-        ERROR_CODE: 9,
+        HTTP_CODE: 200,
+        ERROR_CODE: 10,
         INFO: "password_reset_phone_mismatch"
     },
     COULD_NOT_RESET_PASSWORD: {
-        ERROR_CODE: 10,
+        HTTP_CODE: 200,
+        ERROR_CODE: 11,
         INFO: "could_not_reset_password"
     },
     INVALID_PHONE: {
-        ERROR_CODE: 400,
+        HTTP_CODE: 200,
+        ERROR_CODE: 12,
         INFO: "invalid_phone"
+    },
+    INVALID_AUTH_KEY: {
+        HTTP_CODE: 403,
+        ERROR: 13,
+        INFO: "invalid_auth_key"
+    },
+    MISSING_AUTH_KEY: {
+        HTTP_CODE: 401,
+        ERROR: 14,
+        INFO: "missing_auth_key"
+    },
+    INVALID_SPOT_ID: {
+        HTTP_CODE: 404,
+        ERROR_CODE: 15,
+        INFO: "invalid_spot_id"
     }
 });
