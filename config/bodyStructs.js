dec96dec55365c65492ea25ce284663ab372f561
Fedor Paretsky <me@f3d0r.com>
2018-11-06 15:02:26 -0800

    Remove outdated scripts/packages/references

--- config/bodyStructs.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "/parking": {
-        "/get_status_bbox": ['sw', ['lng', 'lat'], 'ne', ['lng', 'lat']],
-        "/get_status_radius": ['', ['lng', 'lat']],
-        "get_min_size_parking": ['', ['lng', 'lat']]
-    },
-}
