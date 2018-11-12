9155b3baf4a4ffba1dead442c5857c9c60ec6773
Fedor Paretsky <me@f3d0r.com>
2018-11-12 00:10:00 -0800

    Re-add slack error,fix logging bugs

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ cluster(function (worker) {
     app.use(responseTime());
     app.use(morgan(loggingFormat, {
         skip: function (req, res) {
-            if (req.url == '/ping' || req.url == '/') {
+            if ((req.url.length >= 1 && req.url.charAt(req.url.length - 1) == '/') || (req.url.length >= 1 && req.url.substring(req.url.length - 4, req.url.length) == 'ping')) {
                 return true;
             } else {
                 return false;
@@ -99,8 +99,8 @@ cluster(function (worker) {
     app.use(haltOnTimedout);
     app.use(errorHandler);
     app.use(haltOnTimedout);
-    // app.use(sendSlackError);
-    // app.use(haltOnTimedout);
+    app.use(sendSlackError);
+    app.use(haltOnTimedout);
 
     function errorHandler(error, req, res, next) {
         var url = process.env.BASE_URL + req.originalUrl;
@@ -122,13 +122,13 @@ cluster(function (worker) {
         next(error);
     }
 
-    // function sendSlackError(error, req) {
-    //     var message = "aspace Backend Error Notification\n" + "Error: " + JSON.stringify(error) + "\nreq: " + req.url;
-    //     webhook.send(message, function (error, res) {
-    //         if (error)
-    //             console.log('Error: ', error);
-    //     });
-    // }
+    function sendSlackError(error, req) {
+        var message = "aspace Backend Error Notification\n" + "Error: " + JSON.stringify(error) + "\nreq: " + req.url;
+        webhook.send(message, function (error, res) {
+            if (error)
+                console.log('Error: ', error);
+        });
+    }
 
     function haltOnTimedout(req, res, next) {
         if (!req.timedout)
